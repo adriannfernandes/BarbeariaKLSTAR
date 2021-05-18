13a4a2eab10b44da9221896ad2a210657046ab71
josehailton <jose.hailton@dcx.ufpb.br>
2021-05-17 21:02:04 -0300

    classe produto

--- br.ufpb.barbeariaKlsatr/src/main/java/br/ufpb/barbeariaKlstar/entidades/Produto.java
+++ br.ufpb.barbeariaKlsatr/src/main/java/br/ufpb/barbeariaKlstar/entidades/Produto.java
@@ -2,4 +2,30 @@ package br.ufpb.barbeariaKlstar.entidades;
 
 public class Produto {
 
+    private String name;
+    private String price;
+
+    public Produto(String name, String price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public Produto() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
 }
